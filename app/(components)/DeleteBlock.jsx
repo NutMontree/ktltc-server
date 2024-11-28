f700472ac27bfac23a9186bbf7a0e87f09093d2c
NutMontree <nutmontree29@gmail.com>
2024-11-28 12:44:35 +0700

    'v1'

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -7,7 +7,7 @@ const DeleteBlock = ({ id }) => {
   const router = useRouter();
 
   const deleteTicket = async () => {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    const res = await fetch(`https://ktltcp.vercel.app/api/Tickets/${id}`, {
       method: "DELETE",
     });
     if (res.ok) {
