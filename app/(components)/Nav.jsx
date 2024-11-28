09d30ae56bd6c71f6d6328545912cbaf0a37bdaa
NutMontree <nutmontree29@gmail.com>
2024-11-28 12:32:54 +0700

    'v1'

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 const Nav = () => {
   return (
     <>
-      <div>NavBar Page</div>
+      <div className="flex items-center space-x-4">
+        <Link href="/">
+          <div icon={faHome} className="icon">
+            Home
+          </div>
+        </Link>
+        <Link href="/UserPage/new">
+          <div icon={faTicket} className="icon">
+            เพิ่มข้อมูล
+          </div>
+        </Link>
+      </div>
     </>
   );
 };
